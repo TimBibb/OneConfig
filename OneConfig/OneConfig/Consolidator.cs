42c0564661508f2be26120bfe203bc6fa2e30ef6
TimBibb <timothybibbee@gmail.com>
2018-11-24 00:21:03 -0500

    Initial Consolidator build.
    Simple write to a SaveFile and setting of main games.

---
+++ OneConfig/OneConfig/Consolidator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace OneConfig
+{
+    public class Consolidator
+    {
+        static List<int> mainGames = new List<int>();
+        List<List<string>> testData = new List<List<string>>();
+
+        public Consolidator()
+        {
+            testData.Add(new List<string> { "mouse sens", ".25" });
+            testData.Add(new List<string> { "fov", "90" });
+            testData.Add(new List<string> { "divs", "432" });
+            testData.Add(new List<string> { "bind e", "+forward" });
+        }
+
+        public void SetMainGames(List<int> games){
+            for (int i = 0; i < games.Count; i++){
+                mainGames.Add(games[i]);
+            }
+            AppendConfig(testData);
+        }
+        public List<int> GetMainGames(){
+            return mainGames;
+        }
+
+        //On submit of main games
+        private void GetConfig(List<int> games){
+            for (int i = 1; i < games.Count; i++){
+                switch (games[i-1])
+                {
+                    case 1:
+                        //Insert parseBO4
+                        break;
+                    case 2:
+                        //Insert parseBF1
+                        break;
+                    case 3:
+                        //Insert parseBFV
+                        break;
+                    case 4:
+                        //Insert parseOverwatch
+                        break;
+                    case 5:
+                        //Insert parseCSGO...
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void AppendConfig(List<List<string>>parsedData){
+            using (StreamWriter w = File.AppendText("SaveData.txt"))
+            {
+                for (int i = 0; i < parsedData.Count; i++){
+                    for (int j = 0; j < parsedData[i].Count; j++){
+                        if(j + 1 < parsedData[i].Count){
+                            w.Write(parsedData[i][j] + " ");
+                        }else{
+                            w.Write(parsedData[i][j] + "\n");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
--- OneConfig/OneConfig/Program.cs
+++ OneConfig/OneConfig/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace OneConfig
 {
@@ -6,7 +8,17 @@ namespace OneConfig
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Consolidator cons = new Consolidator();
+            List<int> consList = new List<int>();
+
+            Console.WriteLine("Input the corresponding numbers of 3 of your most played games in order of most played to least:");
+            Console.WriteLine("1. Black Ops 4");
+            Console.WriteLine("2. Battlefield 1");
+            Console.WriteLine("3. Battlefield V");
+            Console.WriteLine("4. Overwatch");
+            Console.WriteLine("5. CS:GO");
+            for (int i = 0; i < 3; i++){consList.Add(int.Parse(Console.ReadLine()));}
+            cons.SetMainGames(consList);
         }
     }
 }
